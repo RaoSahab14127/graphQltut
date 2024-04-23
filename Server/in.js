430154019f3a7a0bb233cff6e137742159b5a091
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-04-23 00:42:32 -0700

    workiing on graphql

---
+++ Server/in.js
@@ -0,0 +1,40 @@
+
+import express  from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import  cors from "cors";
+import   axios  from "axios";
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+
+
+        type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+        }
+
+        type Query {
+            getTodos: [Todo]
+        }
+
+    `,
+    resolvers: {
+     
+    },
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
+}
+
+startServer();
---
+++ Server/index.js
@@ -0,0 +1,36 @@
+import express  from "express";
+import {ApolloServer} from '@apollo/server'
+import {expressMiddleware} from '@apollo/server/express4'
+import cors from "cors"
+import bodyParser from "body-parser"
+
+async function startServer(){
+    const app = express()
+    const server = new ApolloServer({
+        typeDefs:`
+        type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+        }
+        type Query {
+                getTodos:[Todo]
+        }
+        `,
+        resolvers:{}
+    })
+
+    app.use(bodyParser.json())
+    app.use(cors)
+
+    await server.start()
+
+    app.use("/graphql", expressMiddleware(server))
+
+    app.listen(8080, ()=>{
+        console.log("Myserver is started")
+    })
+
+
+}
+startServer()
