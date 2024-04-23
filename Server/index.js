409f34e793cb4f34502d64d3a888254583f4f776
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-04-23 00:48:48 -0700

    done

--- Server/index.js
+++ Server/index.js
@@ -1,36 +1,35 @@
 import express  from "express";
-import {ApolloServer} from '@apollo/server'
-import {expressMiddleware} from '@apollo/server/express4'
-import cors from "cors"
-import bodyParser from "body-parser"
-
-async function startServer(){
-    const app = express()
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import  cors from "cors";
+import bodyParser from "body-parser";
+import   axios  from "axios";
+async function startServer() {
+    const app = express();
     const server = new ApolloServer({
-        typeDefs:`
-        type Todo {
-            id: ID!
-            title: String!
-            completed: Boolean
-        }
-        type Query {
-                getTodos:[Todo]
-        }
+        typeDefs: `
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean
+            }
+    
+            type Query {
+                getTodos: [Todo]
+            }
+    
         `,
-        resolvers:{}
-    })
+        resolvers: {},
+      });
 
-    app.use(bodyParser.json())
-    app.use(cors)
+      app.use(bodyParser.json());
+      app.use(cors());
 
-    await server.start()
+      await server.start();
 
-    app.use("/graphql", expressMiddleware(server))
+      app.use("/graphql", expressMiddleware(server));
 
-    app.listen(8080, ()=>{
-        console.log("Myserver is started")
-    })
-
-
-}
-startServer()
+      app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
+    }
+    
+    startServer();
